a3ce54eb90ee4c545426006cec9818ee13d455eb
KhaledBenAbderrahim <74714706+KhaledBenAbderrahim@users.noreply.github.com>
2021-08-31 12:49:26 +0200

    3.0

--- insta_insights/Ins/urls.py
+++ insta_insights/Ins/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.dashboard,name='Home')
+    path('',views.dashboard,name='Home'),
+    path('account',views.erreichte_Konten,name='Account'),
+    path('content',views.content_Interaktionen,name='Content')
 ]
--- insta_insights/Ins/views.py
+++ insta_insights/Ins/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request,'Ins/dashboard.html')
+
+def erreichte_Konten(request):
+    return render(request,'Ins/Erreichte_Konten.html')
+
+def content_Interaktionen(request):
+    return render(request ,'Ins/Content-Interaktionen.html')
