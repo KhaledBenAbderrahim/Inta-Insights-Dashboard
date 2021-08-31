ca1d4fa60c3c61fc5a4af1e7c67b58f0f3799bf2
KhaledBenAbderrahim <74714706+KhaledBenAbderrahim@users.noreply.github.com>
2021-08-31 22:52:27 +0200

    Zielgruppe,Page

--- insta_insights/Ins/urls.py
+++ insta_insights/Ins/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.dashboard,name='Home'),
     path('account',views.erreichte_Konten,name='Account'),
-    path('content',views.content_Interaktionen,name='Content')
+    path('content',views.content_Interaktionen,name='Content'),
+    path('group',views.zielGruppe,name='Group')
 ]
--- insta_insights/Ins/views.py
+++ insta_insights/Ins/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import plotly.express as px
+
 
 # Create your views here.
 
@@ -7,7 +9,38 @@ def dashboard(request):
     return render(request,'Ins/dashboard.html')
 
 def erreichte_Konten(request):
-    return render(request,'Ins/Erreichte_Konten.html')
+    df = px.data.tips()
+
+    #Graph-1 -----------------------------------------------------------------------------------------------------
+    fig = px.pie(df, values='tip', names='day', color='day',
+                 color_discrete_map={'Thur': 'lightcyan',
+                                     'Fri': 'cyan',
+                                     'Sat': 'royalblue',
+                                     'Sun': 'darkblue'})
+
+    #Graph-1 ------------------------------------------------------------------------------------------------------
+    data = dict(
+        number=[39, 27.4, 20.6, 11, 2],
+        stage=["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"])
+    fig_1 = px.funnel(data, x='number', y='stage')
+
+    #Graph-2 ------------------------------------------------------------------------------------------------------
+    df2 = px.data.tips()
+
+    fig_2 = px.density_contour(df2, x="total_bill", y="tip")
+    fig_2.update_traces(contours_coloring="fill", contours_showlabels=True)
+
+
+
+    graph=fig.to_html(full_html=False)
+    graph_2= fig_1.to_html(full_html=False)
+    graph_3= fig_2.to_html(full_html=False)
+    return render(request,'Ins/Erreichte_Konten.html', context={'plot_div': graph,
+                                                                'plot_div2':graph_2,
+                                                                'plot_div3':graph_3})
 
 def content_Interaktionen(request):
     return render(request ,'Ins/Content-Interaktionen.html')
+
+def zielGruppe(request):
+    return render(request,'Ins/Zielgruppe.html')
